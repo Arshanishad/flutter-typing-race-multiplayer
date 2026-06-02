02a61dfb96255107111866110f40074ed4a151b9
arshanishad <arshanishad5@gmail.com>
2026-06-02 11:07:11 +0530

    index.js file

--- server/index.js
+++ server/index.js
@@ -1,42 +1,48 @@
 //IMPORTS
+//require  is inbuilt funtion to import files 
+//Bring express into my file 
+//express is a framework of nodejs without express nodejs is difficult
 const express = require('express');
+//nodejs already has http module (built-in) we use to create server 
 const http = require('http');
+//connect mongodb,create schemas,store data 
 const mongoose = require('mongoose');
-var io=require('socket.io')(server);
-require("dotenv").config();
+const { Socket } = require('socket.io');
+//http create server and socket io uses that server for real-time
+const io = require (Socket.io)(server);
+require('dotenv').config();
 
-// create a server
+//Server
+// Create my backend application using Express
 const app = express();
-const port = process.env.PORT || 3000;
-var server = http.createServer(app);
-var io = require("socket.io")(server);
+const port=process.env.PORT||3000;
+var server=http.createServer(app);
+var io =require('socket.io')(server);
 
-//middle ware
+//middleware
+//Whenever request comes, convert JSON data into JavaScript object
 app.use(express.json());
-//FRONTEND->MIDDLEWARE->BACKEND
-//EVERYTHING IN JAVASCRIPT IS AN OBJECT
 
 //connect to mongodb
-const DB = process.env.MONGO_URI;
+//Get MongoDB connection URL from .env file
+//DB =your database link 
+const DB=process.env.MONGO_URI;
+//on() listen for an event 
+io.on('connection',(Socket)=>{
+console.log(Socket.id);
+Socket.on('test',(data)=>{
+console.log(data);
+});
+});
 
-//listening to the socket io event s from the client(flutter code)
-io.on('connection',(socket)=>{
-    console.log(socket.id);
-    socket.on('test',(data)=>{
-        console.log(data);
-    });
-})
-
-mongoose.connect((DB)).then(() => {
+mongoose.connect((DB)).then(()=>{
     console.log('Connection Successful');
-}).catch((e) => {
+}).catch((e)=>{
     console.log(e);
 });
 
-//listen to server
-server.listen(port, "0.0.0.0", () => {
-    console.log(`Server started and running on port ${port}`);
+//listen to server 
+server.listen(port,"0.0.0.0",()=>{
+    console.log(`server started and running on port ${port}`);
 });
-
-
 
