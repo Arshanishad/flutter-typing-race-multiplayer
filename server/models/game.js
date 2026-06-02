52273b3822c22dc8cc1f0d563e085df7117fad85
arshanishad <arshanishad5@gmail.com>
2026-06-02 15:38:35 +0530

    game and player file schema create

--- server/models/game.js
+++ server/models/game.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
+const playerSchema = require('./player');
 const gameschema = new mongoose.schema({
- words:[
-    {
-        type:String,
+    words: [
+        {
+            type: String,
 
-    }
- ],
- players:[],
+        }
+    ],
+    players: [playerSchema],
+    isJoin: {
+        type: Boolean,
+        default: true,
+    },
+    isOver: {
+        type: Boolean,
+        default: false,
+    },
 });
--- server/models/player.js
+++ server/models/player.js
@@ -1,1 +1,23 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
+const playerSchema= new mongoose.Schema({
+nickname :{
+    type:String
+},
+currentWordIndex:{
+    type:Number,
+    default:0,
+},
+WPM :{
+    type:Number,
+    default:-1,
+},
+SocketID :{
+    type:String,
+},
+isPartyLeader :{
+    type:Boolean,
+    default:false,
+}
+});
+
+module.exports=playerSchema;
